92d3293711db0c4f31805fdcad782656f92f3ca7
yykdev <assavictory@gmail.com>
2019-09-04 18:27:25 +0900

    페이지 라우터 기본 셋팅

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
 import { ApolloProvider } from "react-apollo";
 import client from "./apolloClient";
+import Home from "./Home";
+import Detail from "./Detail";
 
-function App() {
-    return (
-        <ApolloProvider client={client}>
-            <div className="App"></div>
-        </ApolloProvider>
-    );
+class App extends Component {
+    render() {
+        return (
+            <ApolloProvider client={client}>
+                <Router>
+                    <div>
+                        <Route exact={true} path={"/"} component={Home} />
+                        <Route path={"/details/:movieId"} component={Detail} />
+                    </div>
+                </Router>
+            </ApolloProvider>
+        );
+    }
 }
 
 export default App;
