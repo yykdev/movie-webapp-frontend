16447efe61e1e03ce24af9aa0e44ce0ab13837bb
yykdev <assavictory@gmail.com>
2019-09-05 16:51:45 +0900

    apollo 쿼리를 이용하여 graphql 서버 API로 화면에 데이터 출력

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,20 @@
 import React from "react";
+import { Query } from "react-apollo";
+import { HOME_PAGE } from "./queries";
 
-const Home = () => <div>Home</div>;
+const Home = () => (
+    <Query query={HOME_PAGE}>
+        {({ loading, data, error }) => {
+            if (loading) return <span>loading</span>;
+            if (error) return <span>something happend</span>;
+            console.log(data);
+            return data.movies.map(movie => (
+                <h2 key={movie.id}>
+                    {movie.title} / {movie.rating}
+                </h2>
+            ));
+        }}
+    </Query>
+);
 
 export default Home;
---
+++ src/queries.js
@@ -0,0 +1,12 @@
+import gql from "graphql-tag";
+
+export const HOME_PAGE = gql`
+    {
+        movies(limit: 10, rating: 8.5) {
+            id
+            title
+            genres
+            rating
+        }
+    }
+`;
